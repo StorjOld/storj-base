0a6e570bec484ffab665ce6bb9f7cfd331cc7fd4
Bryan White <bryanchriswhite@gmail.com>
2016-10-19 12:10:48 +0200

    rename and enhance makefiles

---
+++ makefiles/build.mk
@@ -0,0 +1,40 @@
+.SILENT:
+.DEFAULT:
+
+help: using longdesc
+
+using:
+	#TODO: change `using` text based on set provider
+	echo "using docker provider"
+
+shortdesc:
+	echo 'build	- build the devops container' | fold -s
+
+longdesc:
+	echo 'Usage: `make build`' | fold -s
+	echo ''
+	echo 'Build the devops container. This container is what runs thor commands passed to it by make' | fold -s
+	echo ''
+
+# Default goal name is `run` instead of matching all, to reduce complexity
+
+run: ensure_devops_base
+	if [ ! $(word 2, $(MAKECMDGOALS)) ]; then \
+		docker-compose -f dockerfiles/thor.yml build thor; \
+	fi \
+
+ensure_devops_base:
+	if ! docker images |grep -q devops-base; then \
+		docker build -t devops-base -f dockerfiles/devops-base.dockerfile .; \
+	fi
+
+ensure_thor: ensure_devops_base
+	if ! docker images |grep -q dockerfiles_thor; then \
+		docker-compose -f dockerfiles/thor.yml build thor; \
+	fi
+
+%:
+	:
+# all goals are phony, this ensures that if there is a
+# directory with the same name as a goal, it still works
+.PHONY: all
---
+++ makefiles/pry.mk
@@ -0,0 +1,29 @@
+.SILENT:
+.DEFAULT:
+
+help: using longdesc
+
+using:
+	#TODO: change `using` text based on set provider
+	echo "using docker provider"
+
+shortdesc:
+	echo 'pry	- start a ruby REPL using pry' | fold -s
+
+longdesc:
+	echo 'Usage: `make pry`' | fold -s
+	echo ''
+	echo 'Start a ruby REPL using [pry](http://pryrepl.org)' | fold -s
+	echo ''
+
+# Default goal name is `run` instead of matching all, to reduce complexity
+
+run:
+	$(MAKE) build ensure_thor
+	docker run --rm -it dockerfiles_thor pry;
+
+%:
+	:
+# all goals are phony, this ensures that if there is a
+# directory with the same name as a goal, it still works
+.PHONY: all
